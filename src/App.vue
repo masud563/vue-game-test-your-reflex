76f952eadb8533ef4a77c4291e490de0411c6daa
masud563 <sydmasud563@gmail.com>
2021-12-05 17:29:26 +0600

    bare bone version is done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Test Your Reflex</h1>
+<button @click="start" :disabled="isPlaying">Play</button> <br>
+<Block v-if="isPlaying" :delay="delay" @end="endGame" />
+<p v-if="showResult">Reactiontime : {{ reactionTime }} ms</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+  },
+  data() {
+    return{
+      isPlaying: false,
+      delay: null,
+      reactionTime: null,
+      showResult: false
+    }
+  },
+  methods: {
+    start() {
+      this.showResult = false
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+    },
+
+    endGame(reactionTime) {
+      this.reactionTime = reactionTime
+      this.showResult = true
+      this.isPlaying = false
+    }
   }
 }
 </script>
---
+++ src/components/Block.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click me</div>
+</template>
+
+<script>
+export default {
+
+  props: ['delay'],
+
+  data() {
+    return{
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+
+    }
+  },
+
+  mounted() {
+    setTimeout(()=>{
+      this.showBlock = true
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      },10)
+    },this.delay)
+  },
+
+  methods: {
+
+    stopTimer() {
+      clearInterval(this.timer)
+      this.showBlock = false
+      this.$emit('end',this.reactionTime)
+    }
+  }
+
+
+}
+</script>
+
+<style>
+  .block {
+    display: inline-block;
+    background-color: rgb(40, 153, 138);
+    border-radius: 20px;
+    margin: 60px auto;
+    padding: 100px 150px ;
+    color: white;
+  }
+</style>
